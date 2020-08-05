763b99d243d36367e7bb7931e442324fad5979f3
Ademayowa adeniyi <ademayowaadeniyi@gmail.com>
2020-08-05 19:37:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Netflix Movie Application
+
+> Video streaming application
+
+## Quick Start
+
+```bash
+# Install client dependencies
+cd netflix-movie-app
+npm install
+npm start
+```
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div className='container-fluid bg-dark'>
+      <h1 className='display-4 text-center text-white mt-5'>Team-React-O</h1>
+    </div>
+  );
+};
+
+export default Landing;
