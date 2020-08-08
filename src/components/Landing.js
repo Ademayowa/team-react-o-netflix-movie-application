305c1ad36d86f85c9017c606bdacd628870eca7a
talk2mat2 <talk2mat2@yahoo.com>
2020-08-08 16:23:20 +0100

    code formated

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Footer from './footer/index'
+import React from "react";
+import Footer from "./footer/index";
 
 const Landing = () => {
   return (
-<React.Fragment>  
-    <div className='container-fluid bg-dark'>
-      <h1 className='display-4 text-center text-white mt-5'>Team-React-O</h1>
-     
-    </div>
-    <Footer />
-  </React.Fragment>
+    <React.Fragment>
+      <div className="container-fluid bg-dark">
+        <h1 className="display-4 text-center text-white mt-5">Team-React-O</h1>
+      </div>
+      <Footer />
+    </React.Fragment>
   );
 };
 
