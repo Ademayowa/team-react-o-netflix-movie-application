c70b8b576e7da973f1127cf3dc30c1441e626097
Pablo <royadeveloper01@gmail.com>
2020-08-09 14:33:27 +0000

    still on it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Landing from './components/Landing';
-import Login from './components/Login';
+import Login from './components/Login/Login';
 
 const App = () => {
   return (
