fb4a2427f546f3b9602141cf68829d1096fccd50
Sifon Isaac <syfonisaac@gmail.com>
2020-08-09 13:35:29 +0100

    watchFaq

--- .vscode/record/src.record/App.js.record/08-09-2020 01-34-36pm.86.js
+++ .vscode/record/src.record/App.js.record/08-09-2020 01-34-36pm.86.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Landing from './components/Landing';
-import Watch from './components/Watch/Watch';
 
 const App = () => {
   return (
     <div className='App'>
       <Landing />
-      <Watch/>
+      
     </div>
   );
 };
---
+++ .vscode/record/src.record/components.record/Watch.record/Watch.js.record/08-09-2020 01-33-54pm.3.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+
+import './Watch.css'
+
+const Watch = () => {
+  return ( <
+    div className = "bg-black" >
+    <
+    div className = "container watch-content" >
+    <
+    Grid container spacing = {
+      2
+    } >
+    <
+    Grid item md = {
+      6
+    }
+    lg = {
+      6
+    }
+    xs = {
+      12
+    } >
+    <
+    div className = "content-pad" >
+    <
+    h1 className = "main-title" > Watch Everywhere < /h1> <
+    h3 className = "subtitle" > Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more. < /h3> <
+    /div> <
+    /Grid> <
+    Grid item md = {
+      6
+    }
+    lg = {
+      6
+    }
+    xs = {
+      12
+    } >
+    <
+    div >
+    <
+    img src = {
+      require("../../images/watch.png")
+    }
+    className = "watch-img" / >
+    <
+    /div> <
+    /Grid>
+
+    <
+    /Grid> <
+    /div> <
+    /div>
+  )
+}
+export default Watch
