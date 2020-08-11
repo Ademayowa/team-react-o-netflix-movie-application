414827f59de12fd86f15344d2cc905b4aca58afe
Adamu Abubakar <adamuabu@gmail.com>
2020-08-11 12:49:11 +0100

    signup planform pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Landing from './components/Landing';
-import SignupPlanform from './components/Signup-planform/Signup-planform';
 
 const App = () => {
   return (
     <div className='App'>
       <Landing />
-      <SignupPlanform />
     </div>
   );
 };
